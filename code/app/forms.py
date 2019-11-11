265811565a2115ef9030a49231ec910f86f912a5
CByrne12 <57369768+CByrne12@users.noreply.github.com>
2019-11-11 14:57:44 -0600

    Update forms.py

--- code/app/forms.py
+++ code/app/forms.py
@@ -65,15 +65,12 @@ class RemoveCartItemForm(forms.ModelForm):
 
 # form for confirming that a user wants to checkout
 class ConfirmCheckoutForm(forms.ModelForm):
-
     confirm = forms.BooleanField()
-
-
-
+    
     class Meta:
         model = NewUser
-        fields = (
+        fields = [
             'address',
             'credit_card',
-        )
+        ]
 
