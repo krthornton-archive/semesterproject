36a34fa3477acb8cae91a6eeaef1757b01a5eebb
Kaleb Thornton <krt237@msstate.edu>
2019-11-14 23:59:02 -0600

    Cleaned things up and added some styling to more pages
    - Changed how messages are display. They're not as cool but they are
      now consistent.
    - Added cards to the shopping cart page to display items.
    - Shopping cart page now displays total, subtotal, tax, etc.
    - Item description page now shows a disabled "Add to Cart" button
      when the user is not logged in.
    - Spiffied up the view account, login, and register pages.

--- code/app/urls.py
+++ code/app/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('browse', views.browse_items, name='browse'),
     re_path(r'^item/(?P<slug>[a-z\-0-9]+)/$', views.item_description, name='description'),
     path('view_cart', views.view_cart, name='view_cart'),
+    path('update_cart', views.update_cart, name='update_cart'),
     path('checkout', views.checkout, name='checkout'),
     path('view_account', views.view_account, name='view_account'),
     path('update_account', views.update_account_info, name='update_account'),
     path('change_password', views.change_password, name='change_password'),
+    path('login', views.login_user, name='login'),
     path('register', views.register, name='register'),
 ]
